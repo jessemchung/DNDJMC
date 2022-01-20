761914373c6cf6f6b0e23b93f761aa7748459aae
Jesse Chung <jessemchung1@gmail.com>
2022-01-20 02:28:57 -0500

    creeps add fix

---
+++ client/src/components/Ryuutama/Generic/RyuutamaTextField.jsx
@@ -0,0 +1,27 @@
+"use strict";
+//purpose, to add a creep to the menu
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RyuutamaTextField = void 0;
+var react_1 = require("react");
+var randomColor = require('randomcolor'); // import the script
+var React = require("react");
+var TextField_1 = require("@mui/material/TextField");
+//card likely needs to have flex to split it in a half
+function RyuutamaTextField(props) {
+    var _a = (0, react_1.useState)(false), open = _a[0], setOpen = _a[1];
+    var _b = (0, react_1.useState)({}), creepInfo = _b[0], setCreepInfo = _b[1];
+    var handleClickOpen = function () {
+        setOpen(true);
+    };
+    var handleClose = function () {
+        setOpen(false);
+    };
+    // <Grid container spacing={1} columns={10}>
+    return (<>
+
+          <TextField_1.default autoFocus margin="dense" id={props.name} name='name' label="Name" type="text" fullWidth variant="standard"/>
+
+    </>);
+}
+exports.RyuutamaTextField = RyuutamaTextField;
+//# sourceMappingURL=RyuutamaTextField.jsx.map
---
+++ client/src/components/Ryuutama/Generic/RyuutamaTextField.tsx
@@ -0,0 +1,64 @@
+//purpose, to add a creep to the menu
+
+import { useState, useEffect } from 'react';
+var randomColor = require('randomcolor'); // import the script
+import * as React from 'react'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import axios from 'axios';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { purple } from '@mui/material/colors';
+
+
+interface Props {
+  name: string
+  label: string
+
+}
+
+//card likely needs to have flex to split it in a half
+export function RyuutamaTextField(props: Props) {
+  const [open, setOpen] = useState<boolean>(false);
+  const [creepInfo, setCreepInfo] = useState({});
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  // <Grid container spacing={1} columns={10}>
+
+  return (
+    <>
+
+          <TextField
+            autoFocus
+            margin="dense"
+            id={props.name}
+            name='name'
+            label="Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+
+    </>
+  )
+}
+
