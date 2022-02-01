1e8105c13516f237033cb9d7f80fc7781297a59e
Jesse Chung <jessemchung1@gmail.com>
2022-02-01 11:51:54 -0500

    Setting up user context

--- client/src/components/Ryuutama/Ryuutama.tsx
+++ client/src/components/Ryuutama/Ryuutama.tsx
@@ -10,11 +10,14 @@ import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import MainStage from './Stage/MainStage.jsx'
 import FreepsMain from './Stage/Pieces/Freeps/FreepsMain.jsx'
 import CreepsMain from './Stage/Pieces/Creeps/CreepsMain.jsx'
-import {CreepsSampleData} from './Stage/Pieces/Creeps/CreepsSampleData.jsx'
-import {FreepsSampleData} from './Stage/Pieces/Freeps/FreepsSampleData.jsx'
+import { CreepsSampleData } from './Stage/Pieces/Creeps/CreepsSampleData.jsx'
+import { FreepsSampleData } from './Stage/Pieces/Freeps/FreepsSampleData.jsx'
 
-import {CreepsCardData, FreepsCardData} from './Stage/Pieces/Common/_Types.jsx'
+import { CreepsCardData, FreepsCardData } from './Stage/Pieces/Common/_Types.jsx'
 import { Button } from '@mui/material';
+import {createContext} from 'react';
+
+const UserContext = createContext(null);
 
 
 const quickTest = <MainStage key={'MainStage'} />
@@ -45,95 +48,98 @@ export default function Ryuutama() {
 
   }
 
-  const initiativeCheck = ()=> {
+  const initiativeCheck = () => {
     //this will check initiative.  A button must be somewhere to also help this with a reset.  Perhaps
     // a crude float or something.  Perhaps in the bottom part of the ryuutama thingx
 
     let found = false;
-    for (let i = initiative; i>0; i--) {
+    for (let i = initiative; i > 0; i--) {
       console.log(i);
     }
   }
-  
+
 
   const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
     lineHeight: '10px',
-    
+
   }));
 
   const darkTheme = createTheme({ palette: { mode: 'dark' } });
   const lightTheme = createTheme({ palette: { mode: 'light' } });
 
-// I would suspect we need to consider where initiative is decided.  It must start with the highest one I think.  If that is the case
-// then both values should start here I suppose, then it picks starting from the highest number
+  // I would suspect we need to consider where initiative is decided.  It must start with the highest one I think.  If that is the case
+  // then both values should start here I suppose, then it picks starting from the highest number
 
-//initiative will need to be tracked so that the next lowest one will be picked.
+  //initiative will need to be tracked so that the next lowest one will be picked.
 
-// a roll can be given I suppose.  
+  // a roll can be given I suppose.  
 
-//a dice roller on top to be implemented first
+  //a dice roller on top to be implemented first
   return (
     <>
 
+      <UserContext.Provider value={{  }}>
 
-      <Paper>        
-        <Box>
-          <Grid container spacing={2}>
-            {[lightTheme].map((theme, index) => (
-              <Grid item xs={6} sm={6} columns={6} key={index}>
-                <ThemeProvider theme={theme}>
-                  <Box
-                    sx={{
-                      p: 1,
-                      bgcolor: 'background.default',
-                      display: 'grid',
-                      gridTemplateColumns: { md: '1fr 4fr 1fr' },
-                      gap: 2,
-                    }}
-                  >
-
-
-                    <Item>
-                      <div>Freeps</div>
-
-                      <FreepsMain key={'FreepsMain'} indexPieces={indexFreeps} setIndexPieces={setIndexFreeps} fullDataFreeps={fullDataFreeps} setFullDataFreeps={setFullDataFreeps} />
-
-                    </Item>
-
-                    <Item>
-                      
-                      <div>Stage</div>
-
-                      {quickTest}
-
-                    </Item>
-
-                    <Item><div>Creeps </div>
-
-                    <CreepsMain key={'CreepsMain'} fullDataCreeps={fullDataCreeps} setFullDataCreeps={setFullDataCreeps}/>
-
-                    </Item>
-
-                  </Box>
-                </ThemeProvider>
-              </Grid>
-            ))}
-          </Grid>
+        <Paper>
+          <Box>
+            <Grid container spacing={2}>
+              {[lightTheme].map((theme, index) => (
+                <Grid item xs={6} sm={6} columns={6} key={index}>
+                  <ThemeProvider theme={theme}>
+                    <Box
+                      sx={{
+                        p: 1,
+                        bgcolor: 'background.default',
+                        display: 'grid',
+                        gridTemplateColumns: { md: '1fr 4fr 1fr' },
+                        gap: 2,
+                      }}
+                    >
 
 
+                      <Item>
+                        <div>Freeps</div>
 
+                        <FreepsMain key={'FreepsMain'} indexPieces={indexFreeps} setIndexPieces={setIndexFreeps} fullDataFreeps={fullDataFreeps} setFullDataFreeps={setFullDataFreeps} />
 
+                      </Item>
 
-        </Box>
+                      <Item>
+
+                        <div>Stage</div>
+
+                        {quickTest}
+
+                      </Item>
+
+                      <Item><div>Creeps </div>
+
+                        <CreepsMain key={'CreepsMain'} fullDataCreeps={fullDataCreeps} setFullDataCreeps={setFullDataCreeps} />
+
+                      </Item>
+
+                    </Box>
+                  </ThemeProvider>
+                </Grid>
+              ))}
+            </Grid>
 
 
 
-      </Paper>
 
-      <Button onClick={testClick}>Test Button</Button>
+
+          </Box>
+
+
+
+        </Paper>
+
+        <Button onClick={testClick}>Test Button</Button>
+      </UserContext.Provider >
+
     </>
   )
 }
