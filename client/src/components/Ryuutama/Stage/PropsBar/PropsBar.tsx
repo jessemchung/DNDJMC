beb006f6b6c1edd3cfe45a7f7315d0878cb8ebb5
Jesse Chung <jessemchung1@gmail.com>
2022-02-02 01:34:51 -0500

    clean up delete

--- client/src/components/Ryuutama/Stage/PropsBar/PropsBar.tsx
+++ client/src/components/Ryuutama/Stage/PropsBar/PropsBar.tsx
@@ -1,16 +1,8 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 var randomColor = require('randomcolor'); // import the script
 import * as React from 'react'
-import * as THREE from "three";
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import PropsBarIndividualCard from './PropsBarIndividualCard.jsx';
 import UserContext from '../../UserContext.jsx';
 
