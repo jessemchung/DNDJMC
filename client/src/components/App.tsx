5736da4d3f9564a583f4b0bc637fa0d8ef52c280
Jesse Chung <jessemchung1@gmail.com>
2024-10-08 14:52:57 -0400

    small fixes to title appearance

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,11 +4,11 @@ import { Outlet, Link } from 'react-router-dom';
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1 className="title">Welcome</h1>
       <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
-        <Link to="/blog">Blog</Link> | {' '}
-        <Link to="/ryuutama">Ryuutama</Link> | {' '}
-        <Link to="/ryuutama">Postcard Maker</Link> | {' '}
+        <Link className="nav-option" to="/blog">Blog</Link> | {' '}
+        <Link className="nav-option" to="/ryuutama">Ryuutama</Link> | {' '}
+        <Link className="nav-option" to="/ryuutama">Postcard Maker</Link> | {' '}
         {/* <Link to="/AboutMyself">About Me</Link>         */}
       </nav>
       <Outlet />
